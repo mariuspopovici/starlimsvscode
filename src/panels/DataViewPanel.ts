7bbd8528d0c29ef4619019facb153edd11702937
Marius Popovici <marius@popovici.net>
2023-07-08 21:28:32 -0400

    Added data source results viewer.

---
+++ src/panels/DataViewPanel.ts
@@ -0,0 +1,147 @@
+import * as vscode from "vscode";
+import { getNonce } from "../utilities/getNonce";
+import { getUri } from "../utilities/getUri";
+
+export class DataViewPanel {
+  public static currentPanel: DataViewPanel | undefined;
+  private readonly _panel: vscode.WebviewPanel;
+  private _disposables: vscode.Disposable[] = [];
+  private _data: string;
+  private _name: string;
+
+  private constructor(
+    panel: vscode.WebviewPanel,
+    extensionUri: vscode.Uri,
+    payload: any
+  ) {
+    this._panel = panel;
+    this._data = payload.data;
+    this._name = payload.name.slice("starlims:".length);
+
+    // Set an event listener to listen for when the panel is disposed (i.e. when the user closes
+    // the panel or when the panel is closed programmatically)
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // Set the HTML content for the webview panel
+    this._panel.webview.html = this._getWebviewContent(
+      this._panel.webview,
+      extensionUri
+    );
+
+    // Set an event listener to listen for messages passed from the webview context
+    this._setWebviewMessageListener(this._panel.webview);
+  }
+
+  public static render(extensionUri: vscode.Uri, payload: any) {
+    if (DataViewPanel.currentPanel) {
+      DataViewPanel.currentPanel._panel.reveal(vscode.ViewColumn.One);
+      DataViewPanel.currentPanel._name = payload.name.slice("starlims:".length);
+      DataViewPanel.currentPanel._data = payload.data;
+    } else {
+      const panel = vscode.window.createWebviewPanel(
+        "data-source-results",
+        "Data Source Results",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+          localResourceRoots: [vscode.Uri.joinPath(extensionUri, "dist")],
+        }
+      );
+
+      DataViewPanel.currentPanel = new DataViewPanel(
+        panel,
+        extensionUri,
+        payload
+      );
+    }
+  }
+
+  public dispose() {
+    DataViewPanel.currentPanel = undefined;
+
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+
+  /**
+   * Defines and returns the HTML that should be rendered within the webview panel.
+   *
+   * @remarks This is also the place where *references* to CSS and JavaScript files
+   * are created and inserted into the webview HTML.
+   *
+   * @param webview A reference to the extension webview
+   * @param extensionUri The URI of the directory containing the extension
+   * @returns A template string literal containing the HTML that should be
+   * rendered within the webview panel
+   */
+  private _getWebviewContent(
+    webview: vscode.Webview,
+    extensionUri: vscode.Uri
+  ) {
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+
+    const nonce = getNonce();
+    const webviewUri = getUri(webview, extensionUri, ["dist", "webview.js"]);
+    const styleUri = getUri(webview, extensionUri, ["dist", "style.css"]);
+
+    const html = /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+          <link rel="stylesheet" href="${styleUri}">
+          <title>Data Source Results</title>
+        </head>
+        <body>
+          <h1 id="title"></h1>
+          <section class="results-grid">
+            <vscode-data-grid id="data-source-grid" 
+            generate-header="sticky" aria-label="Sticky Header" aria-label="Data Source Results"></vscode-data-grid>
+          </section>
+          
+          <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
+        </body>
+      </html>
+    `;
+
+    return html;
+  }
+
+  /**
+   * Sets up an event listener to listen for messages passed from the webview context and
+   * executes code based on the message that is recieved.
+   *
+   * @param webview A reference to the extension webview
+   */
+  private _setWebviewMessageListener(webview: vscode.Webview) {
+    webview.onDidReceiveMessage(
+      (message: any) => {
+        const command = message.command;
+        const data = message.data;
+        switch (command) {
+          case "requestDataSourceResultsData":
+            // Code that should run in response to the hello message command
+            webview.postMessage({
+              command: "receiveDataSourceResultData",
+              payload: this._data,
+              name: this._name,
+            });
+            break;
+          case "hello":
+            vscode.window.showInformationMessage(data);
+            break;
+        }
+      },
+      undefined,
+      this._disposables
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,87 @@
 //@ts-check
 
-'use strict';
+"use strict";
 
-const path = require('path');
+const path = require("path");
+const copyPlugin = require("copy-webpack-plugin");
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
-const extensionConfig = {
-  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-  output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2'
-  },
+const baseConfig = {
+  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
   externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js", ".css"],
+  },
+  devtool: "nosources-source-map",
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
+        use: [{ loader: "ts-loader" }],
+      },
+      // Allow importing CSS modules:
+      {
+        test: /\.css$/,
         use: [
+          "style-loader",
           {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
   },
 };
-module.exports = [ extensionConfig ];
+
+/** @type WebpackConfig */
+const extensionConfig = {
+  ...baseConfig,
+  target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+};
+
+// Config for webview source code (to be run in a web-based context)
+/** @type WebpackConfig */
+const webviewConfig = {
+  ...baseConfig,
+  target: ["web", "es2020"],
+  entry: "./src/webview/main.ts",
+  experiments: { outputModule: true },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "webview.js",
+    libraryTarget: "module",
+    chunkFormat: "module",
+  },
+  plugins: [
+    new copyPlugin({
+      patterns: [
+        {
+          from: "./src/webview/style.css",
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
+};
+module.exports = [extensionConfig, webviewConfig];
