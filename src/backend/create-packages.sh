8b327a0b6dd5848b7084516f03f7ae18d5bd35fc
Marius Popovici <marius.popovici@starlims.com>
2023-07-31 17:59:58 -0400

    Added version and import package endpoints.

--- src/backend/create-packages.sh
+++ src/backend/create-packages.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
+# Get the new version from package.json
+new_version=$(grep -E '"version":' ../../package.json | awk -F '"' '{print $4}' | sed 's/^v//')
+
+# Use sed to replace the sVersion value in the Version endpoint script
+printf  "Patching Version.srvscr with version $new_version from package.json\n"
+version_script_file="./SCM_API/Server Scripts/SCM_API/Version.srvscr"
+sed -i -r "s/(sVersion := \")([^\"]*)(\")/\1$new_version\3/" "$version_script_file"
+
+printf "Generating SCM_API.sdp\n"
+
+# Package the backend scripts into a .sdp file
 [ -e SCM_API.zip ] && rm SCM_API.zip
 cd SCM_API/
 zip -r SCM_API.zip . -r
