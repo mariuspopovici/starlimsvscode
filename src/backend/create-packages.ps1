1e4b2b9105930dd1b9008f69c8403ce9bff12f22
Marius Popovici <marius.popovici@starlims.com>
2023-07-31 18:47:56 -0400

    Updated .ps1

--- src/backend/create-packages.ps1
+++ src/backend/create-packages.ps1
@@ -7,12 +7,13 @@ $jsonData = Get-Content -Raw -Path "..\..\package.json" | ConvertFrom-Json
 $new_version = $jsonData.version
 
 # patch the version endpoint script to return the same value
-Write-Host "Patching Version.srvscr with version $new_version from package.json"
+Write-Host "Patching Version.srvscr with version $new_version from package.json ..."
 $version_script_file = "SCM_API\Server Scripts\SCM_API\Version.srvscr"
 $content = Get-Content -Path "$version_script_file"
 $content = $content -replace '(sVersion := ")[^"]*(")', "`${1}$new_version`$2"
 Set-Content -Path $version_script_file -Value $content
 
+Write-Host "Generating .sdp file ..."
 # create the .sdp package, if exists, overwrite the old zip file
 Compress-Archive -Force -Path .\SCM_API\* -DestinationPath .\SCM_API.zip
 
@@ -23,3 +24,4 @@ if (Test-Path .\SCM_API.sdp) {
 
 # rename the zip file to sdp
 Rename-Item .\SCM_API.zip SCM_API.sdp 
+Write-Host "Done."
